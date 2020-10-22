e05311a8b35f4b287d8a6b4ee3874c58947dbaca
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 12:13:31 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    strList = []
+    while(True):
+      strInput = input()
+      if(strInput==""):
+        break
+      strList.append(strInput)
+
+    for strinT in strList:
+      pieces = strinT.split(" ")
+      for substrinT in pieces:
+        if("av" in substrinT):
+          print(substrinT)
 
 if __name__ == '__main__':
     main()
